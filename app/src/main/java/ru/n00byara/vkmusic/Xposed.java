e0e3b7f4c754971cfa5c0b5f681558c8054463e7
mikhail <aminovmikl.01@gmail.com>
2023-02-01 22:35:07 +0300

    init Xposed && Dexplore api

---
+++ app/src/main/java/ru/n00byara/vkmusic/Xposed.java
@@ -0,0 +1,12 @@
+package ru.n00byara.vkmusic;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class Xposed implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam){
+        
+    }
+}
